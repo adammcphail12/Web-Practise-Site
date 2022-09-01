3775955a513faf6bb42ecc44f88e071bcf6b555d
adammcphail12 <99302328+adammcphail12@users.noreply.github.com>
2022-09-01 13:28:19 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name = "description" content= "add content">
+    <meta name = "keywords" content = "add keywords seperate with commas">
+    <meta name = "author" content = "add your name">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+
+    <title>Book Reviews</title>
+
+    <link rel = "stylesheet" href = "css/bookstyle.css">
+    
+</head>
+<body>
+    <div class="wrapper">
+        <div class ="box logo">
+            Logo
+        </div> <!-- Logo Ends -->
+
+        <div class ="box banner">
+            Banner
+        </div> <!-- Banner Ends -->
+
+        <div class ="box nav">
+            Nav
+        </div> <!-- Nav Ends -->
+
+        <div class ="box main">
+            Main
+        </div> <!-- Main Ends -->
+
+        <div class ="box side">
+            Side
+        </div> <!-- Side Ends -->
+
+        <div class ="box footer">
+            Footer
+        </div> <!-- Footer Ends -->
+
+    </div> <!-- Wrapper Ends -->
+</body>
